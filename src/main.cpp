40ae724c060051c4ef7f00153735b35747f5a918
heretic.spork <heretic.spork@gmail.com>
2025-06-11 15:00:48 -0500

    defined project requiements in the main source file ('src/main.cpp')

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <iostream>
+#include <bits/stdc++.h>
+#include <fstream>
+using namespace std;
+
+//Markov model tasks:
+
+
+// 1. identify the source file to read: user specified?  Dropped into a folder? Character limit?
+// 2. Read the file
+// 3. Get the size of the file based on the number of characters
+// 4. Get user input seed size (a number 0-10)
+// 5. Store the character seeds as specified by the seed size along with the following character.
+// 6. Define the starting seed.
+// 7. Take the seed string and review the stored data.  Calculate the probability of the next character based on the stored data.
+// 8. Generate the next character.  Continue generating until the end of the original text is reached
+// 9. Store the generated text and characters.
+// 10. Return the generated text to the user.
+// 11. Write unit tests
+// 12. Handle errors.
