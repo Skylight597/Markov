824bdbcab0d1a19e5bfaa0b3d39f2cc0cedcb469
root <cpyrtle@proton.me>
2025-06-13 14:49:56 -0500

    Began writing a second function to map seed strings to the characters that follow them.

--- src/main.cpp
+++ src/main.cpp
@@ -39,14 +39,52 @@ vector<char> readFile(string fileName){
 }
 
 
+void mapFile (vector<char> content, int seed){
+
+    int correctseed = seed - 1;
+
+    map<string, vector<char>> fileMap;
+    
+    if(correctseed < 1){
+
+        correctseed++;
+
+    }
+    
+    int loopcount = content.size() - correctseed;
+
+   for(int i=0; i < loopcount; i++){
+        string key(content.begin()+i, content.begin()+correctseed);
+        
+   }
+    
+}
+
+
 
 int main(){
 
+    string filePath;
+    int seed;
+
+    cout << "This program autogenerates text using a Markov model.  To begin, please input the path to your seed file." << endl;
+
+    cin >> filePath;
+
+    cout << "Thanks!  Now please indicate how many characters the model should use for analysis (specify a number 1-10)." << endl;
+
+    cin >> seed;
+
     vector<char> printOut = readFile("textFile.txt");
 
-    for(int i=0; i< printOut.size(); i++){
+    string subvector(printOut.begin()+0, printOut.begin() + 3);
 
-        cout << printOut[i] << endl;
+    vector<char>::iterator values;
+    values = search(printOut.begin(), printOut.end(), subvector.begin(), subvector.end());
+
+    for(int i=0; i < subvector.size(); i++){
+
+        cout << values[i];
 
     }
 
